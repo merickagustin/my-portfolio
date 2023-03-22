14dc79624d770bbb7988ebefd4cb5b0a8e0fa093
merickagustin <merickic.agustin@gmail.com>
2023-03-22 21:27:19 +0800

    Add new update for Skill section

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,8 +30,10 @@ function About() {
           <h1>About</h1>
           <p>
             My name is&nbsp;
-            <strong className={classes.name}>Merick Icy Agustin</strong>, I've
-            been in the I.T industry for&nbsp;
+            <strong className={classes.name + " text-primary"}>
+              Merick Icy Agustin
+            </strong>
+            , I've been in the I.T industry for&nbsp;
             <strong>{curYear - startYear}</strong> years. I graduated at Notre
             Dame of Dadiangas Universisty with a Degree of Bachelor Science in
             Computer Science.
@@ -39,6 +41,7 @@ function About() {
           <p>I'm currently exploring new techs in building web applications.</p>
           <div className="about-links d-flex justify-content-center align-items-center gap-1">
             <a href="#" className={classes["download-cv"]}>
+              {/* <a href="#" className="bg-primary link-light rounded-pill"> */}
               Download CV
             </a>
             <a href="#">View Projects &rarr;</a>
@@ -88,12 +91,12 @@ function About() {
       </div>
       <div className="pb-1 px-1 px-md-0">
         <div className="container pt-3 px-0 ">
-          <h1>Skills/Services</h1>
+          <h1>Skills</h1>
         </div>
         <ul
           className={
             classes["skill-list"] +
-            " mt-2 d-flex flex-row gap-1 justify-content-center"
+            " mt-2 d-flex flex-column flex-lg-row gap-1 justify-content-center gap-2 gap-lg-4"
           }
         >
           <li
@@ -102,7 +105,7 @@ function About() {
             }
           >
             <svg
-              className={classes["skill-icon"]}
+              className={classes["skill-icon"] + "  d-flex align-self-center"}
               xmlns="http://www.w3.org/2000/svg"
               data-name="Layer 1"
               viewBox="0 0 48 48"
@@ -119,7 +122,7 @@ function About() {
                 className="color000000 svgShape"
               ></path>
             </svg>
-            <span>C#</span>
+            <span className="d-flex justify-content-center fw-normal">C#</span>
           </li>
           <li
             className={
@@ -127,7 +130,7 @@ function About() {
             }
           >
             <svg
-              className={classes["skill-icon"]}
+              className={classes["skill-icon"] + " d-flex align-self-center"}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 128 128"
             >
@@ -137,7 +140,9 @@ function About() {
                 d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zm-46.885-37.793h-11.709l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"
               />
             </svg>
-            <span>JavaScript</span>
+            <span className="d-flex justify-content-center fw-normal">
+              JavaScript
+            </span>
           </li>
           <li
             className={
@@ -145,7 +150,7 @@ function About() {
             }
           >
             <svg
-              className={classes["skill-icon"]}
+              className={classes["skill-icon"] + " d-flex align-self-center"}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 128 128"
             >
@@ -154,7 +159,9 @@ function About() {
                 d="M75.701 65.603c-2.334-.768-5.694-.603-10.08-.603h-17.621v23h18.844c2.944 0 5.012-.315 6.203-.535 2.099-.376 3.854-1.104 5.264-1.982 1.409-.876 2.568-2.205 3.478-3.881.908-1.676 1.363-3.637 1.363-5.83 0-2.568-.658-4.54-1.975-6.436-1.316-1.896-3.141-2.965-5.476-3.733zM73.282 55.087c2.317-.688 4.064-1.89 5.239-3.487 1.176-1.598 1.763-3.631 1.763-6.044 0-2.286-.549-4.314-1.646-6.054s-2.662-2.413-4.699-3.056c-2.037-.641-5.53-.446-10.48-.446h-15.459v20h16.587c4.042 0 6.939-.38 8.695-.913zM126 18.625c0-9.182-7.443-16.625-16.625-16.625h-91.75c-9.182 0-16.625 7.443-16.625 16.625v91.75c0 9.182 7.443 16.625 16.625 16.625h91.75c9.182 0 16.625-7.443 16.625-16.625v-91.75zm-35.447 66.12c-1.362 2.773-3.047 4.911-5.052 6.415-2.006 1.504-4.521 2.78-7.544 3.548-3.022.769-6.728 1.292-11.113 1.292h-27.844v-69h27.42c5.264 0 9.485.609 12.665 2.002 3.181 1.395 5.671 3.497 7.474 6.395 1.801 2.898 2.702 5.907 2.702 9.071 0 2.945-.8 5.708-2.397 8.308-1.598 2.602-4.011 4.694-7.237 6.292 4.166 1.222 7.37 3.304 9.61 6.248 2.24 2.945 3.36 6.422 3.36 10.432 0 3.227-.681 6.225-2.044 8.997z"
               />
             </svg>
-            <span>Bootstrap</span>
+            <span className="d-flex justify-content-center fw-normal">
+              Bootstrap
+            </span>
           </li>
           <li
             className={
@@ -162,7 +169,7 @@ function About() {
             }
           >
             <svg
-              className={classes["skill-icon"]}
+              className={classes["skill-icon"] + " d-flex align-self-center"}
               xmlns="http://www.w3.org/2000/svg"
               data-name="Layer 1"
               viewBox="0 0 24 24"
@@ -174,7 +181,9 @@ function About() {
                 className="color000000 svgShape"
               ></path>
             </svg>
-            <span>HTML5</span>
+            <span className="d-flex justify-content-center fw-normal">
+              HTML5
+            </span>
           </li>
           <li
             className={
@@ -182,7 +191,7 @@ function About() {
             }
           >
             <svg
-              className={classes["skill-icon"]}
+              className={classes["skill-icon"] + " d-flex align-self-center"}
               xmlns="http://www.w3.org/2000/svg"
               data-name="Layer 1"
               viewBox="0 0 24 24"
@@ -194,7 +203,7 @@ function About() {
                 className="color000000 svgShape"
               ></path>
             </svg>
-            <span>CSS</span>
+            <span className="d-flex justify-content-center fw-normal">CSS</span>
           </li>
         </ul>
       </div>
