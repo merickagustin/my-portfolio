1a1c4a473da12b8e9f5013bc0e00a432b89207fb
merickagustin <merickic.agustin@gmail.com>
2023-04-01 21:24:00 +0800

    Add new Project page content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,13 @@
+import classes from "../css/Projects.module.css";
+
 function Projects() {
-  return <div>testing</div>;
+  return (
+    <div className="row justify-content-center">
+      <div className={classes[""]}>
+        <h1>Projects</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
