9aeff0d0a23c81ea3d48fcd7e9a631dff68835fb
merickagustin <merickic.agustin@gmail.com>
2023-04-03 23:08:02 +0800

    Add new layout for project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,38 +6,52 @@ const projectCards = [
     projectDate: "(March 2023)",
     projectDescription:
       "Building a responsive website from mobile first to desktop display.",
+    sourceCodeLink: "https://github.com/merickagustin/omnifood-website-project",
+    sourceWebsite: "https://omnifood-merick.netlify.app/",
     techs: ["HTML5", "JavaScript", "CSS"],
   },
 ];
 
 const renderProjectCards = (projectCards) => {
   return projectCards.map((projectCard) => (
-    <div className="project-card">
+    <div className="project-card shadow p-3">
       <div className="project-heading">
         <p className="project-title">{projectCard.projectTitle}</p>
         <p className="project-date">{projectCard.projectDate}</p>
       </div>
-      <p className="project-description">{projectCard.projectDescription}</p>
       <ul className="tech-tags">
         {projectCard.techs.map((tech) => (
           <li className="tech-tag-item">{tech}</li>
         ))}
       </ul>
+      <p className="project-description">{projectCard.projectDescription}</p>
+      <div className="source-links d-flex gap-2">
+        <a className="web-link button--link" href={projectCard.sourceWebsite}>
+          View Website
+        </a>
+        <a
+          className="source-link button--link"
+          href={projectCard.sourceCodeLink}
+        >
+          View Projects
+        </a>
+      </div>
     </div>
   ));
 };
 
 function Projects() {
   return (
-    <div className="row justify-content-center">
-      <div className="px-1 py-3 px-lg-3 py-xl-4 row justify-content-center">
+    <section className="row justify-content-center">
+      <div className="px-1 py-3 px-lg-5 py-xl-4 row justify-content-center">
         <div className="container pt-3 px-0">
           <h1>Projects</h1>
-          <div className="projects"></div>
-          {renderProjectCards(projectCards)}
+          <div className={classes.projects}>
+            {renderProjectCards(projectCards)}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
