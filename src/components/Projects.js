85e532a60d90cb30b92316840b29eb1e29fd10a0
merickagustin <merickic.agustin@gmail.com>
2023-04-05 15:58:57 +0800

    Add new contents for project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,8 +13,7 @@ const projectCards = [
   {
     projectTitle: "Portfolio Website",
     projectDate: "(April 2023)",
-    projectDescription:
-      "Building a my own portfolio website to showcase my projects.",
+    projectDescription: "My own portfolio website to showcase my projects.",
     sourceCodeLink: "https://github.com/merickagustin/my-portfolio",
     sourceWebsite: "",
     techs: ["HTML5", "JavaScript", "CSS", "ReactJs", "BootStrap5"],
@@ -23,49 +22,23 @@ const projectCards = [
 
 const renderProjectCards = (projectCards) => {
   return projectCards.map((projectCard) => (
-    // <div className="project-card shadow col p-2">
-    //   <div className="project-heading text-light">
-    //     <p className="project-title">{projectCard.projectTitle}</p>
-    //     <p className="project-date">{projectCard.projectDate}</p>
-    //   </div>
-    //   <ul className={classes["tech-tags"] + " d-flex flex-row gap-1"}>
-    //     {projectCard.techs.map((tech) => (
-    //       <li className="tech-tag-item">
-    //         <span>{tech}</span>
-    //       </li>
-    //     ))}
-    //   </ul>
-    //   <p className="project-description">{projectCard.projectDescription}</p>
-    //   <div className="source-links d-flex gap-2">
-    //     <a className="web-link button--link" href={projectCard.sourceWebsite}>
-    //       View Website
-    //     </a>
-    //     <a
-    //       className="source-link button--link"
-    //       href={projectCard.sourceCodeLink}
-    //     >
-    //       View Projects
-    //     </a>
-    //   </div>
-    // </div>
-    <div className="col col-sm-3">
+    <div className="col-10 col-sm-8 col-lg-12 col-xl-6">
       <div class="card border-primary shadow">
-        <div className="card-header d-flex flex-row gap-1 align-items-center">
-          <p className={classes["project-title"] + " text-primary"}>
-            {projectCard.projectTitle}
+        <div className="card-header d-flex flex-column flex-lg-row gap-1 align-items-center">
+          <p className={classes["project-title"]}>{projectCard.projectTitle}</p>
+          <p className={classes["project-date"] + " text-primary"}>
+            {projectCard.projectDate}
           </p>
-          <p className={classes["project-date"]}>{projectCard.projectDate}</p>
         </div>
         <div class="card-body">
           <p className="card-text">{projectCard.projectDescription}</p>
-          <ul className={classes["tech-tags"] + " d-flex flex-row gap-1 mb-1"}>
+          <div className={classes["tech-tags"]}>
             {projectCard.techs.map((tech) => (
-              <li className="tech-tag-item">
-                <span>{tech}</span>
-              </li>
+              <span>{tech}</span>
             ))}
-          </ul>
-          <div className="source-links d-flex gap-2">
+          </div>
+
+          <div className="source-links d-flex flex-column flex-md-row gap-2 mt-2">
             {projectCard.sourceWebsite !== "" ? (
               <a
                 className={classes["link-highlight"]}
@@ -91,11 +64,11 @@ const renderProjectCards = (projectCards) => {
 
 function Projects() {
   return (
-    <section className="row justify-content-center">
+    <section className="container-fluid justify-content-center">
       <div className="px-1 py-3 px-lg-5 py-xl-4 row justify-content-center">
         <div className="container pt-3 px-0">
           <h1>Projects</h1>
-          <div className="projects row row-cols-1 row-cols-xl-2">
+          <div className="row g-1 justify-content-center">
             {renderProjectCards(projectCards)}
           </div>
         </div>
