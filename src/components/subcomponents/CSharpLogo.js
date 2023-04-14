7494fb204af0f3dd94615542d758edaec2228aff
merickagustin <merickic.agustin@gmail.com>
2023-04-14 17:48:52 +0800

    New updates

---
+++ src/components/subcomponents/CSharpLogo.js
@@ -0,0 +1,36 @@
+function CSharpLogo(props) {
+  return (
+    <svg
+      className={props.className}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0,0,256,256"
+      width="256px"
+      height="256px"
+      fillRule="nonzero"
+    >
+      <g
+        fill="#014687"
+        fillRule="nonzero"
+        stroke="none"
+        strokeWidth="1"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit="10"
+        strokeDasharray=""
+        strokeDashoffset="0"
+        fontFamily="none"
+        fontWeight="none"
+        fontSize="none"
+        textAnchor="none"
+        style={{ mixBlendMode: "normal" }}
+      >
+        <g transform="scale(8,8)">
+          <path d="M16,2.88281l-13,6.5v13.23437l13,6.5l13,-6.5v-13.23437zM16,5.11719l11,5.5v10.76562l-11,5.5l-11,-5.5v-10.76562zM13,10c-3.30078,0 -6,2.69922 -6,6c0,3.30078 2.69922,6 6,6c1.5,0 2.90234,-0.60156 3.90234,-1.5l-1.70312,-1.19922c-0.60156,0.39844 -1.39844,0.69922 -2.19922,0.69922c-2.19922,0 -4,-1.80078 -4,-4c0,-2.19922 1.80078,-4 4,-4c0.80078,0 1.59766,0.30078 2.19922,0.69922l1.70313,-1.19922c-1,-0.89844 -2.40234,-1.5 -3.90234,-1.5zM18,12v1h-1v2h1v2h-1v2h1v1h2v-1h2v1h2v-1h1v-2h-1v-2h1v-2h-1v-1h-2v1h-2v-1zM20,15h2v2h-2z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default CSharpLogo;
---
+++ src/components/subcomponents/HTML5Logo.js
@@ -0,0 +1,36 @@
+function HTML5Logo(props) {
+  return (
+    <svg
+      className={props.className}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0,0,256,256"
+      width="256px"
+      height="256px"
+      fillRule="nonzero"
+    >
+      <g
+        fill="#014687"
+        fillRule="nonzero"
+        stroke="none"
+        strokeWidth="1"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit="10"
+        strokeDasharray=""
+        strokeDashoffset="0"
+        fontFamily="none"
+        fontWeight="none"
+        fontSize="none"
+        textAnchor="none"
+        style={{ mixBlendMode: "normal" }}
+      >
+        <g transform="scale(8,8)">
+          <path d="M5.21875,4l0.09375,1.09375l1.75,19.78125l0.0625,0.6875l0.65625,0.1875l7.9375,2.21875l0.25,0.0625l0.28125,-0.0625l7.9375,-2.1875l0.65625,-0.1875l0.0625,-0.6875l1.78125,-19.8125l0.09375,-1.0625h-1.09375l-19.375,-0.03125zM7.40625,6l17.1875,0.03125l-1.625,18l-6.96875,1.9375l-7,-1.9375zM9.8125,9l0.65625,7.46875h8.5625l-0.28125,3.21875l-2.75,0.75h-0.03125l-2.75,-0.75l-0.15625,-2.0625h-2.5l0.34375,3.96875l5.0625,1.40625h0.03125l5.0625,-1.40625l0.6875,-7.59375h-9.03125l-0.21875,-2.53125h9.46875l0.21875,-2.46875z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default HTML5Logo;
